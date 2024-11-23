2f1215dc0eee6327b9c91afea394b59e41bfe828
theco <thecodingbee.dev@gmail.com>
2024-11-23 14:38:40 +0100

    Updated RecordParamTest

--- personaltracker-backend/src/test/java/RecordParamTest.java
+++ personaltracker-backend/src/test/java/RecordParamTest.java
@@ -1,3 +1,4 @@
+import dto.RecordDTO;
 import enums.Currency;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,23 +8,23 @@ import java.time.LocalDateTime;
 public class RecordParamTest {
     @Test
     public void functionalTest(){
-        Assertions.assertDoesNotThrow(() -> new Record("name", 10, Currency.CZK, LocalDateTime.now(), "note"));
+        Assertions.assertDoesNotThrow(() -> new RecordDTO("name", 10, Currency.CZK, LocalDateTime.now(), "note"));
     }
 
     @Test
     public void negativeValueTest(){
-        Assertions.assertDoesNotThrow(() -> new Record("name", -10, Currency.CZK, LocalDateTime.now(), "note"));
+        Assertions.assertDoesNotThrow(() -> new RecordDTO("name", -10, Currency.CZK, LocalDateTime.now(), "note"));
     }
 
     @Test
     public void nameTest(){
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-                new Record("", 1, Currency.CZK, LocalDateTime.now(), "note"));
+                new RecordDTO("", 1, Currency.CZK, LocalDateTime.now(), "note"));
     }
 
     @Test
     public void zeroValueTest(){
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-                new Record("name", 0, Currency.CZK, LocalDateTime.now(), "note"));
+                new RecordDTO("name", 0, Currency.CZK, LocalDateTime.now(), "note"));
     }
 }
